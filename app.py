25dd3266e0f43514cdc72b42e606cd74e8af2bd7
OgnjenR93 <ognjen@avokado.rs>
2026-08-11 17:44:13 +0200

    Initial Streamlit app

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Bulk PageSpeed Checker",
+    page_icon="⚡",
+    layout="wide"
+)
+
+st.title("Bulk PageSpeed Checker")
+
+st.write(
+    "Bulk PageSpeed and mobile Core Web Vitals analysis."
+)
